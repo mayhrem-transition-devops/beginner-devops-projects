27d8771eeb1e98a165273edfb55812515be12604
mayhrem <renecruz.0711@gmail.com>
2025-11-16 21:00:04 -0600

    Adding app file

--- pro-01-static-site/app/app.py
+++ pro-01-static-site/app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask'
+    version = os.environ.get("APP_VERSION", "1.0.0")
+    return f'<h1>Hello from the DevOps Container</h1><p>Version: {version}</p>'
 
 if __name__== '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
